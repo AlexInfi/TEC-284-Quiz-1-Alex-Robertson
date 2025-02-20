a48ff2985da3959b65af5f2f9ceae31dd46932fc
AlexInfi <rarobe1@ilstu.edu>
2025-02-20 15:40:56 -0600

    Color Mixing
    Creates the functions and loop to color mix. Currently, the red is the only one that works properly, as Green and Blue are inexplicably always emitting HIGH.

--- Quiz-1-Sketch-Alex-Robertson/Quiz-1-Sketch-Alex-Robertson.ino
+++ Quiz-1-Sketch-Alex-Robertson/Quiz-1-Sketch-Alex-Robertson.ino
@@ -9,14 +9,35 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(13) == HIGH) {
-    digitalWrite(7, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(5, LOW);
-  } else {
-    digitalWrite(7, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(5, LOW);
+  if (digitalRead(13) == HIGH) {  // If red pushpin is pressed
+      setRed();
+  } else if (digitalRead(12) == HIGH) { // If Green pushpin is pressed
+      setGreen();
+  } else if (digitalRead(11) == HIGH) { // If Blue pushpin is pressed
+      setBlue();
+  } else {  // If no pushpins are being pressed
+      clear();
   }
+}
 
+// Function to turn the Red LED on
+void setRed() {
+  digitalWrite(7, HIGH);
+}
+
+// Function to turn the Green LED on
+void setGreen() {
+  digitalWrite(6, HIGH);
+}
+
+// Function to turn the Blue LED on
+void setBlue() {
+  digitalWrite(5, HIGH);
+}
+
+// Turns all LEDs off
+void clear() {
+  digitalWrite(7, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(5, LOW);
 }
