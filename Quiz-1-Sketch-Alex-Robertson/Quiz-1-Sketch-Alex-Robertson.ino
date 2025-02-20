372ee5d3b89080b110b939436dcca7c7333871a5
AlexInfi <rarobe1@ilstu.edu>
2025-02-20 14:34:52 -0600

    Red LED Working
    A testing the code and the wiring to make sure the Red Pushpin lights up the Red LED when pressed, and shuts off when let go of.

--- Quiz-1-Sketch-Alex-Robertson/Quiz-1-Sketch-Alex-Robertson.ino
+++ Quiz-1-Sketch-Alex-Robertson/Quiz-1-Sketch-Alex-Robertson.ino
@@ -1,9 +1,22 @@
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(13, INPUT_PULLUP);  // Red Pushpin
+  pinMode(12, INPUT_PULLUP);  // Green Pushpin
+  pinMode(11, INPUT_PULLUP);  // Blue Pushpin
 
+  pinMode (7, OUTPUT);  // Red LED
+  pinMode (6, OUTPUT);  // Green LED
+  pinMode (5, OUTPUT);  // Blue LED
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(13) == HIGH) {
+    digitalWrite(7, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+  } else {
+    digitalWrite(7, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(5, LOW);
+  }
 
 }
